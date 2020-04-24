d6790e50114fce7226820211eaf990f4c8dd2e72
Kusjay <kelechiuma@gmail.com>
2020-04-24 15:23:48 +0100

    Connected to mongo db with mongoose

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -15,3 +19,10 @@ const server = app.listen(
     `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
   )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.messgae}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
