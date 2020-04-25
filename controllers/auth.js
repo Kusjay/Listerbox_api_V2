2fffdb367b482bbc7f2f4f426edd7e22e7510f4e
Kusjay <kelechiuma@gmail.com>
2020-04-25 17:51:16 +0100

    Worked on log out user route and clear cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,6 +46,23 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc    Log user out / clear cookie
+// @route   POST /api/v2/auth/logout
+// @access  Private
+
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    // Expires in 10 sec
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
