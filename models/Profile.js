ea5ff27ac2cd36da293d4a9114f4448278c1d847
Kusjay <kelechiuma@gmail.com>
2020-05-02 18:56:00 +0100

    Created the profile model

---
+++ models/Profile.js
@@ -0,0 +1,99 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
+
+const ProfileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'Please add a profile name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Profile name can not be more than 50 characters']
+  },
+  accountNumber: {
+    type: Number,
+    select: false
+  },
+  bankName: {
+    type: String,
+    select: false
+  },
+  slug: String,
+  description: {
+    type: String,
+    require: [true, 'Please add a description'],
+    maxlength: [500, 'Description can not be more than 500 characters']
+  },
+  phone: {
+    type: String,
+    required: [true, 'Please add a phone number'],
+    maxlength: [20, 'Phone number can not be longer than 20 characters']
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please add a valid email'
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address']
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point']
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
+  photo: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+// Create profile slug from the name
+ProfileSchema.pre('save', function (nex) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & create location field
+ProfileSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
+  next();
+});
+
+module.exports = mongoose.model('Profile', ProfileSchema);
