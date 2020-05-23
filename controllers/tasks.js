98bf7a172da3eec5a1c22a6f730e3358b31a673b
Kusjay <kelechiuma@gmail.com>
2020-05-23 19:02:24 +0100

    Worked on get single task, update task, delete task endpoints

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -36,3 +36,100 @@ exports.addTask = asyncHandler(async (req, res, next) => {
     data: task
   });
 });
+
+// @desc    GET all task
+// @route   GET /api/v2/tasks
+// @route   GET /api/v2/profiles/:profileId/tasks
+// @access  Public
+exports.getTasks = asyncHandler(async (req, res, next) => {
+  if (req.params.profileId) {
+    const tasks = await Task.find({ profile: req.params.profileId });
+
+    return res.status(200).json({
+      success: true,
+      count: tasks.length,
+      data: tasks
+    });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
+});
+
+// @desc    Get single task
+// @route   GET /api/v2/tasks/:id
+// @access  Public
+
+exports.getTask = asyncHandler(async (req, res, next) => {
+  const task = await Task.findById(req.params.id).populate({
+    path: 'profile',
+    select: 'name description'
+  });
+
+  if (!task) {
+    return next(new ErrorResponse(`No task with id of ${req.params.id}`), 404);
+  }
+
+  res.status(200).json({
+    success: true,
+    data: task
+  });
+});
+
+// @desc    Update task
+// @route   PUT /api/v2/tasks/:id
+// @access  Private
+exports.updateTask = asyncHandler(async (req, res, next) => {
+  let task = await Task.findById(req.params.id);
+
+  if (!task) {
+    return next(new ErrorResponse(`No task with id of ${req.params.id}`), 404);
+  }
+
+  // Make sure the user is the owner of the task
+  if (task.user.toString() !== req.user.id && req.user.role !== 'Admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update task ${task._id}`,
+        401
+      )
+    );
+  }
+
+  task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: task
+  });
+});
+
+// @desc    Delete Task
+// @route   DELETE /api/v2/tasks/:id
+// @access  Private
+exports.deleteTask = asyncHandler(async (req, res, next) => {
+  const task = await Task.findById(req.params.id);
+
+  if (!task) {
+    return next(new ErrorResponse(`No task with id of ${req.params.id}`), 404);
+  }
+
+  // Make sure the user is the owner of the task
+  if (task.user.toString() !== req.user.id && req.user.role !== 'Admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete task ${task._id}`,
+        401
+      )
+    );
+  }
+
+  await task.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,34 @@
 const express = require('express');
-const { addTask } = require('../controllers/tasks');
+const {
+  addTask,
+  getTasks,
+  getTask,
+  updateTask,
+  deleteTask
+} = require('../controllers/tasks');
 
 const Task = require('../models/Task');
 
 const router = express.Router({ mergeParams: true });
+
+const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').post(protect, authorize('Tasker', 'Admin'), addTask);
+router
+  .route('/')
+  .post(protect, authorize('Tasker', 'Admin'), addTask)
+  .get(
+    advancedResults(Task, {
+      path: 'profile',
+      select: 'name'
+    }),
+    getTasks
+  );
+
+router
+  .route('/:id')
+  .get(getTask)
+  .put(protect, authorize('Tasker', 'Admin'), updateTask)
+  .delete(protect, authorize('Tasker', 'Admin'), deleteTask);
 
 module.exports = router;
