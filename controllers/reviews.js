3b31f19f332a654e57fb95b3944567ae2c3da1de
Kusjay <kelechiuma@gmail.com>
2020-06-10 08:39:10 +0100

    Allow user and admin view, update and delete reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -45,3 +45,94 @@ exports.addReview = asyncHandler(async (req, res, next) => {
     data: review
   });
 });
+
+// @desc    Get all reviews
+// @route   GET /api/v2/reviews
+// @route   GET /api/v2/tasks/:taskId/reviews
+// @access  Public
+exports.getReviews = asyncHandler(async (req, res, next) => {
+  if (req.params.taskId) {
+    const reviews = await Review.find({ task: req.params.taskId });
+
+    return res.status(200).json({
+      success: true,
+      count: reviews.length,
+      data: reviews
+    });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
+});
+
+// @desc    Get a single review
+// @desc    GET /api/v2/reviews/:id
+// @access  Public
+exports.getReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id).populate({
+    path: 'task',
+    select: 'title'
+  });
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review found with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: review
+  });
+});
+
+// @desc    Update review
+// @route   PUT /api/v2/reviews/:id
+// @access  Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(new ErrorResponse(`No review with id ${req.params.id}`, 404));
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'Admin') {
+    return next(new ErrorResponse(`Not authorized to update review`, 401));
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: review
+  });
+});
+
+// @desc    Delete review
+// @route   DELETE /api/v2/reviews/:id
+// @access  Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findByIdAndUpdate(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure the review belongs to the user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'Admin') {
+    return next(new ErrorResponse(`Not authorized to delete review`, 401));
+  }
+
+  await review.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { addReview } = require('../controllers/reviews');
+const {
+  addReview,
+  getReviews,
+  getReview,
+  updateReview,
+  deleteReview
+} = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 
@@ -8,6 +14,21 @@ const router = express.Router({ mergeParams: true });
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').post(protect, authorize('User', 'Admin'), addReview);
+router
+  .route('/')
+  .post(protect, authorize('User', 'Admin'), addReview)
+  .get(
+    advancedResults(Review, {
+      path: 'task',
+      select: 'title'
+    }),
+    getReviews
+  );
+
+router
+  .route('/:id')
+  .get(getReview)
+  .put(protect, authorize('User', 'Admin'), updateReview)
+  .delete(protect, authorize('Admin'), deleteReview);
 
 module.exports = router;
